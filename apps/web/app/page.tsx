d1481f777df65865418aa697d5c0c5fb11014e23
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-10 21:22:39 +0000

    chore: just testing out husky

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,9 @@ import { Button } from '@repo/ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">Hello</h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">
+        Hello from Turbo App
+      </h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
