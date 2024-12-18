61cb6021efd2cb6bd986b94e2b28b2be7ceaf8e3
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-18 10:55:07 +0000

    build(package.json): adds some handy scripts to run docker from the npm script

--- packages/ui/src/components/custom/Sample/Sample.tsx
+++ packages/ui/src/components/custom/Sample/Sample.tsx
@@ -4,7 +4,7 @@ export const Sample = () => {
   return (
     <div>
       <h1 className="text-8xl text-test-500">This is sample component</h1>
-      <Button variant="default">Sample Button !!</Button>
+      <Button variant="default">Button </Button>
     </div>
   )
 }
