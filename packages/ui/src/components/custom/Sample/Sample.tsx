4eaf02023d49d50abdaabea866aaf5dc7cce1d04
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-18 22:02:40 +0000

    feat(sample): tesing out

--- packages/ui/src/components/custom/Sample/Sample.tsx
+++ packages/ui/src/components/custom/Sample/Sample.tsx
@@ -4,7 +4,7 @@ export const Sample = () => {
   return (
     <div>
       <h1 className="text-8xl text-test-500">
-        Hello from Sample 👋 ! Testings 23
+        Hello from Sample 👋 ! Testings 234
       </h1>
       <Button variant="default">Sample Button !!</Button>
     </div>
