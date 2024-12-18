4c0e0a143cef591cb1ffbaf307fb9432b0109fb0
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-18 17:29:31 +0000

    feat(sample): testing again

--- packages/ui/src/components/custom/Sample/Sample.tsx
+++ packages/ui/src/components/custom/Sample/Sample.tsx
@@ -3,7 +3,9 @@ import { Button } from '../../ui/button'
 export const Sample = () => {
   return (
     <div>
-      <h1 className="text-8xl text-test-500">Hello from Sample 👋 ! Test</h1>
+      <h1 className="text-8xl text-test-500">
+        Hello from Sample 👋 ! Testings 23
+      </h1>
       <Button variant="default">Sample Button !!</Button>
     </div>
   )
