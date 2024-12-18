0de6ff1bf6411ffa6515f02ccf7b48d8fb4a248f
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-18 13:13:24 +0000

    fix(dockerfile): fixes dockerfile for ui and nextjs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="text-9xl text-red-600 bg-test-700">
-        Hello from Turbo App
+        Hello from Turbo App !!!
       </h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
