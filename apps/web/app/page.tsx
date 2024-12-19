7be09fd82d4d9e2d3bca134f313d3ed485af3a5b
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-19 22:30:49 +0000

    fix(page.tsx): just added text to trigger deployment

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,9 +4,7 @@ import { Button } from '@ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">
-        Hello from Turbo App !!!
-      </h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">Hello 🐨</h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
