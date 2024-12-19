391f3dacfd707e2d7ed692fd9576a4fefc25754d
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-19 21:22:30 +0000

    feat(sample): testing new workflow ui

--- packages/ui/src/components/custom/Sample/Sample.tsx
+++ packages/ui/src/components/custom/Sample/Sample.tsx
@@ -3,7 +3,7 @@ import { Button } from '../../ui/button'
 export const Sample = () => {
   return (
     <div>
-      <h1 className="text-8xl text-test-500">🐻 Hello</h1>
+      <h1 className="text-8xl text-test-500">New CI / CD 🐨</h1>
       <Button variant="default">Sample Button !!</Button>
     </div>
   )
