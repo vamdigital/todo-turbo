9ee3abd2e85780d6c1439b777761d8e25032597c
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-19 13:06:21 +0000

    feat(sample): adds bear to make changes

--- packages/ui/src/components/custom/Sample/Sample.tsx
+++ packages/ui/src/components/custom/Sample/Sample.tsx
@@ -3,9 +3,7 @@ import { Button } from '../../ui/button'
 export const Sample = () => {
   return (
     <div>
-      <h1 className="text-8xl text-test-500">
-        New Day - New Challenge Accepted
-      </h1>
+      <h1 className="text-8xl text-test-500">🐻 Hello</h1>
       <Button variant="default">Sample Button !!</Button>
     </div>
   )
