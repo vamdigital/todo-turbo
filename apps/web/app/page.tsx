365d7f7273a777fb3755632e8012517d439a3a04
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-19 22:36:10 +0000

    fix(deploy): fixing vercel usage of npm instead of pnpm

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">Hello 🐨</h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">Hello 🐨 - Koala </h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
