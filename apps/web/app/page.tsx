7360c1f60159ca89830d755eb74bff6223f1e161
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-19 22:52:18 +0000

    fix(deploy): vercel.json

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,9 +4,7 @@ import { Button } from '@ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">
-        Hello 🐨 - Koala - says hello{' '}
-      </h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">🐨 - Emoji galore</h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
