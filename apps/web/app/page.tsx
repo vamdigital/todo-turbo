5976f3eb8ae136052b22702bdf437d94d50d3495
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-20 22:57:15 +0000

    feat(dockerfile): fixing dockerfile for cannot copy pnpm-lock file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">Finally :)</h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">Finally !!! :)</h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
