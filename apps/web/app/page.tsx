682bf04f587d818d213c987e100c362355777329
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2024-12-20 10:37:32 +0000

    fix(page.tsx): trying to see if the pipelines work

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,9 @@ import { Button } from '@ui/components/ui/button'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-9xl text-red-600 bg-test-700">🐨 - Emoji galore</h1>
+      <h1 className="text-9xl text-red-600 bg-test-700">
+        🐨 - Good Morning {Date.now().toLocaleString()}
+      </h1>
       <Button variant="destructive">Destructive</Button>
       <Sample />
     </div>
